84f764e1d56f4ffe9c90ccbee66ea3d7e69e7ae6
unknown <ljeger@student.unipu.hr>
2020-06-25 09:18:13 +0200

    message

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import mongo from 'mongodb';
 import auth from './auth.js';
 
 const app = express(); // instanciranje aplikacije
-const port = process.env.port || 3000; // port na kojem će web server slušati
+const port = process.env.PORT || 3000; // port na kojem će web server slušati
 
 app.use(cors());
 app.use(express.json());
